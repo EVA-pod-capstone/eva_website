77dfec022d093b75cfe0e4e0d5510a2c416a99f7
JuliaDiTomas <111463451+JuliaDiTomas@users.noreply.github.com>
2025-04-10 11:01:19 -0600

    set decimal places in filename

--- upload.php
+++ upload.php
@@ -3,9 +3,12 @@
     $data = file_get_contents('php://input');
     $json_data = json_decode($data);
     $lat = $json_data->latitude;
-    $long = $json_data->longitude;
+    $lng = $json_data->longitude;
+    $lat_formatted = number_format((float)$lat, 4, '.', '');
+    $lng_formatted = number_format((float)$lng, 4, '.', '');
+
     // write the data out to the file
-    $fp = fopen("/var/www/html/uploads/".$lat."_".$long.".json", "wb");
+    $fp = fopen("/var/www/html/uploads/".$lat_formatted."_".$lng_formatted.".json", "wb");
 
     fwrite($fp, $data);
     fclose($fp);
