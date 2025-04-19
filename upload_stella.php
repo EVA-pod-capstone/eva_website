cb02cea78ed0de07b7e4c679ef37967f850e15bb
JuliaDiTomas <111463451+JuliaDiTomas@users.noreply.github.com>
2025-04-19 15:07:34 -0600

    Create upload_stella.php

---
+++ upload_stella.php
@@ -0,0 +1,16 @@
+<?php    
+    // gets entire POST body
+    $data = file_get_contents('php://input');
+    $json_data = json_decode($data);
+    $lat = $json_data->latitude;
+    $lng = $json_data->longitude;
+    $lat_formatted = number_format((float)$lat, 4, '.', '');
+    $lng_formatted = number_format((float)$lng, 4, '.', '');
+
+    // write the data out to the file
+    $fp = fopen("/var/www/html/uploads/".$lat_formatted."_".$lng_formatted."_stella.json", "wb");
+
+    fwrite($fp, $data);
+    fclose($fp);
+?>
+
